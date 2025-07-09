8dbb27c10adaf3153bb50615d458dde36bd82fb4
Reyan Patel <reyanp246@gmail.com>
2025-07-09 03:12:23 +0000

    Made nicer formatting

--- project1.py
+++ project1.py
@@ -1,5 +1,14 @@
 import requests
 
-response = requests.get("https://pokeapi.co/api/v2/pokedex/kanto")
+url = "https://pokeapi.co/api/v2/pokedex/kanto"
 
-print (response.json())
+response = requests.get(url)
+
+data = response.json()
+
+print ("Kanto Pokedex")
+pokemon_entries = data.get("pokemon_entries", [])
+for entry in pokemon_entries:
+  entry_num = entry["entry_number"]
+  name = entry["pokemon_species"]["name"]
+  print(f"{entry_num:03d} - {name.capitalize()}")
