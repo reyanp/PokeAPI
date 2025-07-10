2e744f302fcbfa54c5cc627b3524a1411a5366dc
Reyan Patel <reyanp246@gmail.com>
2025-07-10 18:00:55 +0000

    Added SQLite database

--- project1.py
+++ project1.py
@@ -1,14 +1,38 @@
 import requests
+import pandas as pd
+import sqlalchemy as db
 
 url = "https://pokeapi.co/api/v2/pokedex/kanto"
-
 response = requests.get(url)
-
 data = response.json()
 
-print ("Kanto Pokedex")
 pokemon_entries = data.get("pokemon_entries", [])
-for entry in pokemon_entries:
-  entry_num = entry["entry_number"]
-  name = entry["pokemon_species"]["name"]
-  print(f"{entry_num:03d} - {name.capitalize()}")
+rows = [
+    {
+        "entry_number": entry["entry_number"],
+        "name": entry["pokemon_species"]["name"].capitalize()
+    }
+    for entry in pokemon_entries
+]
+df = pd.DataFrame.from_dict(rows)
+
+print("Kanto Pokedex (DataFrame):")
+print(df.head())
+
+engine = db.create_engine('sqlite:///kanto_pokedex.db')
+
+df.to_sql(
+    'kanto_pokemon', 
+    con=engine, 
+    if_exists='replace', 
+    index=False
+)
+
+with engine.connect() as connection:
+    result = connection.execute(
+        db.text("SELECT * FROM kanto_pokemon;")
+    ).fetchall()
+
+    df2 = pd.DataFrame(result, columns=df.columns)
+    print("\nFetched from SQLite:")
+    print(df2.head())
